ffba13a4f94c5d531ea018864d334472ee0b48e0
mohibul islam <121642208+mohibulsh@users.noreply.github.com>
2023-06-06 01:16:44 +0600

    add the jwt

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
-const express =require('express');
-const app =express()
+const express = require('express');
+const app = express()
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
-const cors =require('cors')
-const port =process.env.PORT || 5000;
+const cors = require('cors')
+const port = process.env.PORT || 5000;
 
 //midelware
 app.use(cors())
 app.use(express.json())
-
-app.get('/',(req,res)=>{
-    res.send('bistro boss is runnig ..........')    
+//jwt verification 
+const verifyjwt =(req,res,next)=>{
+  const authorization =req.headers.authorization;
+  if(!authorization){
+    return res.status(401).send({error:true, maessage:'unauthorized access'})
+  }
+  //bearer token
+  const token =authorization.split(' ')[1];
+  jwt.verify(token,process.env.ACCESS_TOKEN, (err, decoded)=>{
+    if(err){
+      return res.status(401).send({error:true,messange:'unauthorized access'})
+    }
+    req.decoded=decoded;
+    next()
+  });
+}
+app.get('/', (req, res) => {
+  res.send('bistro boss is runnig ..........')
 })
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -28,82 +44,94 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-   // get the all menu data from database
-   const menuCollection =client.db('bristroDb').collection('menu')
-   const reviewCollection =client.db('bristroDb').collection('reviews')
-   app.get('/menu', async(req,res)=>{
-    const result = await menuCollection.find().toArray();
-    res.send(result)
-   })
-   app.get('/reviews', async(req,res)=>{
-    const result = await reviewCollection.find().toArray();
-    res.send(result)
-   })
-  //send the particular data to mongodb
-  const cartCollection =client.db('bristroDb').collection('carts')
-  app.post('/carts',async(req,res)=>{
-    const doc =req.body;
-    const result = await cartCollection.insertOne(doc);
-    res.send(result)
-  })
+    // get the all menu data from database
+    const menuCollection = client.db('bristroDb').collection('menu')
+    const reviewCollection = client.db('bristroDb').collection('reviews')
+    app.get('/menu', async (req, res) => {
+      const result = await menuCollection.find().toArray();
+      res.send(result)
+    })
+    app.get('/reviews', async (req, res) => {
+      const result = await reviewCollection.find().toArray();
+      res.send(result)
+    })
+    //send the particular data to mongodb
+    const cartCollection = client.db('bristroDb').collection('carts')
+    app.post('/carts', async (req, res) => {
+      const doc = req.body;
+      const result = await cartCollection.insertOne(doc);
+      res.send(result)
+    })
 
-  //user create and saved the mongodb database
-  const usersCollection =client.db('bristroDb').collection('users')
-  app.post('/users',async(req,res)=>{
-    const user =req.body;
-    const query ={email:user.eamil} 
-    console.log(query)
-    const existanceUser = await usersCollection.findOne(query.email)
-    console.log(existanceUser)
-    if(existanceUser){
-      return res.send({messange:'user already exists'})
-    }
-    const result =await usersCollection.insertOne(user)
-    res.send(result) 
-  })
-  //find the all users
-  app.get('/users',async(req,res)=>{
-    const result =await usersCollection.find().toArray();
-    res.send(result)
-  })
-  //create a admin users
-  app.patch('/users/admin/:id',async(req,res)=>{
-    const id =req.params.id;
-    const filter ={_id : new ObjectId(id)}
-    const updateDoc = {
-      $set: {
-        role:'admin'
-      },
-    };
-    const result = await usersCollection.updateOne(filter,updateDoc)
-    res.send(result)
-  })
-  //admin delete 
-  app.delete('/users/admin/:id',async(req,res)=>{
-    const id =req.params.id;
-    console.log(id)
-    const query ={_id :new ObjectId(id)}
-    const result =await usersCollection.deleteOne(query);
-    res.send(result)
-  })
-  //get the data depends on email
-  app.get('/carts',async(req,res)=>{
-    const email =req.query.email;
-    if(!email){
-      res.send([])
-    }
-    const query ={email:email};
-    const result =await cartCollection.find(query).toArray();
-    res.send(result)
-  })
-  //delete particular data form mongodb 
-  app.delete('/carts/:id',async(req,res)=>{
-    const id =req.params.id;
-    console.log(id)
-    const query ={_id :new ObjectId(id)}
-    const result =await cartCollection.deleteOne(query);
-    res.send(result)
-  })
+    //user create and saved the mongodb database
+    const usersCollection = client.db('bristroDb').collection('users')
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const query = { email: user.eamil }
+      console.log(query)
+      const existanceUser = await usersCollection.findOne(query.email)
+      console.log(existanceUser)
+      if (existanceUser) {
+        return res.send({ messange: 'user already exists' })
+      }
+      const result = await usersCollection.insertOne(user)
+      res.send(result)
+    })
+    //find the all users
+    app.get('/users', async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result)
+    })
+    //create a admin users
+    app.patch('/users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) }
+      const updateDoc = {
+        $set: {
+          role: 'admin'
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc)
+      res.send(result)
+    })
+    //admin delete 
+    app.delete('/users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      console.log(id)
+      const query = { _id: new ObjectId(id) }
+      const result = await usersCollection.deleteOne(query);
+      res.send(result)
+    })
+    //get the data depends on email
+    app.get('/carts',verifyjwt, async (req, res) => {
+      const email = req.query.email;
+      if (!email) {
+        res.send([])
+      }
+      const decodedEmail=req.decoded.email;
+      if(email !==decodedEmail){
+        return res.status(403).send({error:true, maessage:'unauthorized access'})
+      } 
+      const query = { email: email };
+      const result = await cartCollection.find(query).toArray();
+      res.send(result)
+    })
+    //delete particular data form mongodb 
+    app.delete('/carts/:id', async (req, res) => {
+      const id = req.params.id;
+      console.log(id)
+      const query = { _id: new ObjectId(id) }
+      const result = await cartCollection.deleteOne(query);
+      res.send(result)
+    })
+    //post jwt token
+    app.post('/jwt', (req, res) =>{
+      const user =req.body;
+      const token =jwt.sign(user,process.env.ACCESS_TOKEN,{
+        expiresIn: '1h'
+      })
+      res.send({token})
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
@@ -115,6 +143,6 @@ async function run() {
 }
 run().catch(console.dir);
 
-app.listen(port,()=>{
+app.listen(port, () => {
   console.log(`bristo boss is running on the ${port}`)
 })
